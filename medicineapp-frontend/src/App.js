a477d249583bae1b03b43d1c5ca0249598646af9
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-10-25 13:12:24 +0530

    feat - navigation menu

--- medicineapp-frontend/src/App.js
+++ medicineapp-frontend/src/App.js
@@ -1,32 +1,17 @@
 import React from 'react'
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import About from './pages/About'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
 import Testimonials from './pages/Testimonials'
 import Home from './pages/Home'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <div>
       <Router>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/about'>About</Link>
-          </li>
-          <li>
-            <Link to='/testimonials'>Testimonials</Link>
-          </li>
-          <li>
-            <Link to='/login'>Login</Link>
-          </li>
-          <li>
-            <Link to='/profile'>Profile</Link>
-          </li>
-        </ul>
+        <Header />
         <Switch>
           <Route path='/testimonials'>
             <Testimonials />
--- medicineapp-frontend/src/components/Header.js
+++ medicineapp-frontend/src/components/Header.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <Link class="navbar-brand" to="/">Home</Link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <Link class="nav-link" to="/">Home <span class="sr-only">(current)</span></Link>
+                </li>
+                <li class="nav-item">
+                    <Link class="nav-link" to="/about">About</Link>
+                </li>
+                <li class="nav-item">
+                    <Link class="nav-link" to="/testimonials">Testimonials</Link>
+                </li>
+                <li class="nav-item">
+                    <Link class="nav-link" to="/profile">Profile</Link>
+                </li>
+                <li class="nav-item">
+                    <Link class="nav-link" to="/login">Login</Link>
+                </li>
+            </ul>
+        </div>
+</nav>
+    </>
   );
 }
 
